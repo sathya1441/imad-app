89677c9b6f76da81aa13572e354fcc17e3c1e7a5
sathya1441 <30805259+sathya1441@users.noreply.github.com>
2017-09-17 02:42:05 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/',function(req, res){
+app.get('/article-one',function(req, res){
     res.send('article-one has been started');
 });
 app.get('/ui/style.css', function (req, res) {
